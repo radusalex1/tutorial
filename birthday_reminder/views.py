a717313c2b3b8eea64507f8b79025d54c3f733d3
radusalex1 <137905517+ralexandruserban@users.noreply.github.com>
2023-12-21 17:29:52 +0200

    Update views.py

--- birthday_reminder/views.py
+++ birthday_reminder/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets,status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.core import serializers
 
 from birthday_reminder.serializers import ContactSerializer
 from .models import *
@@ -11,17 +12,13 @@ import datetime
 # Create your views here.
 @api_view(('GET',))
 def birthday_reminder(request):
-    contacts = Contact.objects.all()
-    print(contacts)
-    return Response({"contacts":contacts},status=status.HTTP_404_NOT_FOUND)
-
-    # try:
-    #     contacts = Contact.objects.get(birth_date__day = datetime.datetime.now().day,birth_date__month = datetime.datetime.now().month,active=True)
-    #     return JsonResponse(contacts)
-    # except Contact.DoesNotExist:
-    #     return HttpResponse('Nu e ziua nimanui')
-    # except Exception as e:
-    #     return HttpResponse(e)
+    try:
+        contacts = Contact.objects.get(birth_date__day = datetime.datetime.now().day,birth_date__month = datetime.datetime.now().month,active=True)
+        return JsonResponse(contacts)
+    except Contact.DoesNotExist:
+        return HttpResponse('Nu e ziua nimanui')
+    except Exception as e:
+        return HttpResponse(e)
     
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
