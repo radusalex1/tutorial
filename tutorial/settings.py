f425038347a937771756ddb93449e1da62f99d8f
radusalex1 <radus_alexandru@yahoo.com>
2023-12-22 15:58:51 +0200

    added pg admin db.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['djangotutorial2.azurewebsites.net','localhost']
 
 
+
 # Application definition
 INSTALLED_APPS = [
     'django_crontab',
@@ -77,12 +78,15 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'django-tutorial',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
