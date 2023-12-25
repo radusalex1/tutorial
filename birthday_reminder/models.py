32315008e75e84613acd251142e6cfffcadab261
radusalex1 <radus_alexandru@yahoo.com>
2023-12-25 16:48:35 +0200

    added new field to group

--- birthday_reminder/models.py
+++ birthday_reminder/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Group(models.Model):
     name = models.CharField(max_length=250)
+    active=models.BooleanField(default = False)
 
     def __str__(self) -> str:
         return f'{self.name}'
